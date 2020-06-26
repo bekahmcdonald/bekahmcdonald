dd1c45a75170c2f49a7414db2c5adf2ab7142049
Bekah McDonald <github@bekahmcdonald.com>
2020-06-26 14:12:52 +0100

    Import data from global rest route

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,19 @@
     h1 bekahmcdonald.com
 </template>
 
+<script>
+import Axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await Axios.get(
+      `https://cms.bekahmcdonald.com/wp-json/bm/v1/global`
+    )
+    return data
+  },
+}
+</script>
+
 <style lang="scss" scoped>
 h1 {
   color: blue;
