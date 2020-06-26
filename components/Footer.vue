59ad8bc434eea9e5ec1abf4f8888ae0167efae87
Bekah McDonald <github@bekahmcdonald.com>
2020-06-26 14:10:33 +0100

    Build header and footer

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  footer
+    .o-container
+      p.small &copy; #{ new Date().getFullYear() } Bekah McDonald
+    
+</template>
+
+<style lang="scss" scoped>
+footer {
+  background-color: $cotton;
+  padding: 8px 0 24px;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+  .o-container
+    header
+      nuxt-link(to="/").logo Bekah
+      nav 
+        .link(@click="scrollTo('work')") Work
+        .link(@click="scrollTo('contact')") Contact
+</template>
+
+<script>
+export default {
+  methods: {
+    scrollTo(id) {
+      document.getElementById(id).scrollIntoView({ behavior: 'smooth' })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+
+  @include mq($from: lg) {
+    height: 80px;
+  }
+}
+
+.logo {
+  text-decoration: none;
+  color: inherit;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 1.3px;
+  font-size: 16px;
+  user-select: none;
+
+  @include mq($from: md) {
+    font-size: 18px;
+  }
+
+  @include mq($from: md) {
+    font-size: 24px;
+  }
+}
+
+nav {
+  .link {
+    @extend %label;
+    cursor: pointer;
+    display: inline-block;
+    text-decoration: none;
+    color: inherit;
+    margin-left: 40px;
+    font-weight: 400;
+    border-bottom: solid 1px transparent;
+    padding-bottom: 2px;
+    transition: border-color 0.3s ease-in-out;
+
+    @media (hover: hover) and (pointer: fine) {
+      &:hover {
+        border-bottom-color: currentColor;
+      }
+    }
+  }
+}
+</style>
