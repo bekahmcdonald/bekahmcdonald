76828f55ad3c21ddbbb38ba01395ad4880ba7d99
Bekah McDonald <github@bekahmcdonald.com>
2020-06-28 18:59:43 +0100

    Add meta info and favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,18 +16,40 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Bekah McDonald | Front End Developer based in London',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content:
+          'I’m a front-end developer, with 3+ years of agency experience and a strong background in project management. I’m a fast learner, love teaching myself new things, and bring a project-management level of organisation to everything I do.',
       },
+      { name: 'msapplication-TileColor', content: '#f8c34e' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#f8c34e' },
+
       {
         href:
           'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap',
