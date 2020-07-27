af295d5167293cae4b2fb233245fcc80f52ad4b0
Bekah McDonald <github@bekahmcdonald.com>
2020-07-27 16:25:18 +0100

    Add structured data for better SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,11 +8,19 @@
 <script>
 import TheHeader from '@/components/Header'
 import TheFooter from '@/components/Footer'
+import structuredData from '@/assets/json-ld.json'
 
 export default {
   components: {
     TheHeader,
     TheFooter,
+  },
+  head() {
+    return {
+      script: [
+        { type: 'application/ld+json', src: JSON.stringify(structuredData) },
+      ],
+    }
   },
 }
 </script>
